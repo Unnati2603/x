485c8d6a753c86a2fde832996b8df4fa89492cc3
unnati <unnatichaurasia12@gmail.com>
2024-04-13 05:06:56 +0530

    tweet delete button and function

--- frontend/src/components/Tweet.js
+++ frontend/src/components/Tweet.js
@@ -4,8 +4,51 @@ import Avatar from "react-avatar";
 import { FaRegComment } from "react-icons/fa";
 import { CiHeart } from "react-icons/ci";
 import { CiBookmark } from "react-icons/ci";
+import { MdDeleteOutline } from "react-icons/md";
+
+import axios from "axios";
+import { TWEET_API_END_POINT } from "../utils/constant";
+import { useDispatch, useSelector } from "react-redux";
+import toast from "react-hot-toast";
+import { getRefresh } from "../redux/tweetSlice";
 
 const Tweet = ({ tweet }) => {
+  const { user } = useSelector((store) => store.user);
+  // const dispatch = useDispatch;
+
+  // tweet id and wwho liked or disliked
+  const dispatch = useDispatch();
+  const likeOrDislikeHandler = async (id) => {
+    try {
+      const res = await axios.put(
+        `${TWEET_API_END_POINT}/like/${id}`,
+        { id: user?._id },
+        {
+          withCredentials: true,
+        }
+      );
+      console.log(res);
+      dispatch(getRefresh());
+      toast.success(res.data.message);
+    } catch (error) {
+      toast.success(error.response.data.message);
+      console.log(error);
+    }
+  };
+
+  const deleteTweetHandler = async (id) => {
+    try {
+      axios.defaults.withCredentials = true;
+      const res = await axios.delete(`${TWEET_API_END_POINT}/delete/${id}`);
+      console.log(res);
+      dispatch(getRefresh());
+      toast.success(res.data.message);
+    } catch (error) {
+      toast.success(error.response.data.message);
+      console.log(error);
+    }
+  };
+
   return (
     <div className="border-b border-gray-200">
       <div>
@@ -31,7 +74,10 @@ const Tweet = ({ tweet }) => {
                 <p>0</p>
               </div>
               <div className="flex items-center">
-                <div className="p-2 hover:bg-pink-200 rounded-full cursor-pointer">
+                <div
+                  onClick={() => likeOrDislikeHandler(tweet?._id)}
+                  className="p-2 hover:bg-pink-200 rounded-full cursor-pointer"
+                >
                   <CiHeart size="24px" />
                 </div>
                 <p>{tweet?.like?.length}</p>
@@ -42,6 +88,18 @@ const Tweet = ({ tweet }) => {
                 </div>
                 <p>0</p>
               </div>
+
+              {/* for delete */}
+              {user?._id === tweet?.userId && (
+                <div
+                  onClick={() => deleteTweetHandler(tweet?._id)}
+                  className="flex items-center"
+                >
+                  <div className="p-2 hover:bg-red-300 rounded-full cursor-pointer">
+                    <MdDeleteOutline size="24px" />
+                  </div>
+                </div>
+              )}
             </div>
           </div>
         </div>
